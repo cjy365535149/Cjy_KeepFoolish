391e70330a930e1a6fd71774bdf444fb456d9fa1
Jay Cong <365535149@qq.com>
2019-08-17 23:37:25 +0800

    无法本地调试指定端口！

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ const name = defaultSettings.title || 'vue Element Admin' // page title
 // You can change the port by the following method:
 // port = 9527 npm run dev OR npm run dev --port = 9527
 // const port = process.env.port || process.env.npm_config_port || 9527 // dev port
-const port = 8081// dev port
+const port = 8081// dev port  //端口无法指定！！！！！！！！！！！！！！！！！！
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
   /**
